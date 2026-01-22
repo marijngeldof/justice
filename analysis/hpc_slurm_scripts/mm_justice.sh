144bf5021b17925221797d2072e410a103a3ab58
PBiswas <p.biswas@tudelft.nl>
2026-01-22 14:26:53 +0100

    updated simulation of the IAM with optimized weights

--- analysis/hpc_slurm_scripts/mm_justice.sh
+++ analysis/hpc_slurm_scripts/mm_justice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name="mm10kBorg"
+#SBATCH --job-name="200kMM"
 #SBATCH --partition=memory
-#SBATCH --time=20:00:00
+#SBATCH --time=30:00:00
 #SBATCH --ntasks=45
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=16G
@@ -11,8 +11,9 @@
 
 module load 2025
 module load openmpi
-module load miniconda3
-source "$(conda info --base)/etc/profile.d/conda.sh"
+
+
+source /scratch/$USER/.conda/etc/profile.d/conda.sh
 conda activate justice311
 
 # Prevent threaded BLAS from oversubscribing
@@ -24,13 +25,13 @@ export NUMEXPR_NUM_THREADS=1
 cd "$SLURM_SUBMIT_DIR"
 mkdir -p logs                 # <- ensure logs dir exists
 
-# MS: 1 master + 1 worker (BORG_ISLANDS env not used by MSBorgMOEA, but harmless)
+# MS: 1 master + 1 worker (BORG_ISLANDS env not used by MSBorgMOEA, but harmless) N islands each with K workers, request N*(K+1) + 1 MPI nodes when submitting the job
 export BORG_ISLANDS=4
 
 # Per-run args
-nfe=10000
+nfe=200000
 myswf=0
-seed=69
+seed=555
 scenario_index=2
 
 mpirun -np "$SLURM_NTASKS" python hpc_run.py "$nfe" "$myswf" "$seed" "$scenario_index"
